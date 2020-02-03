388fd0e9b66ff4170806617966fa77bb06223ca1
Russel Pugh <russ_scooby@yahoo.com>
2020-02-03 23:11:28 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -26,7 +26,7 @@ def position_taken?(board, index)
   end
 end
 
-def move(board, index, token = "X" || "O")
+def move(board, index, token = "X")
   board[index] = token
 end
 
@@ -35,7 +35,7 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board, index) == true
-    move(board, index, token = "X" || "O")
+    move(board, index, token = "X")
     display_board(board)
   else
     turn(board)
