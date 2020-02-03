2d57b861fb8dcd45092f2c4b9c624629c232c178
Russel Pugh <russ_scooby@yahoo.com>
2020-02-03 23:19:19 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -34,7 +34,7 @@ end
 
 def turn(board)
   puts "Please ender 1-9:"
-  user_input = gets.strip 
+  user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board, index) == true
     move(board, index, token = "X")
